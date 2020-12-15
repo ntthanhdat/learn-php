b94e353b61de8a488440f0f9535e1b635ceaee35
ntthanhdat <thanhdatnguyenthe@gmail.com>
2020-12-16 00:17:27 +0700

    creat update-topics

--- admin/edittopic.php
+++ admin/edittopic.php
@@ -10,22 +10,26 @@ $result=mysqli_query($conn,$sql);
  if(mysqli_num_rows($result)>0){
     $post=mysqli_fetch_assoc($result);
 }
-
+//print_r($post);
 ?>
 <main class="container">
+
     <div class="row">
         <div class="col-md-12">
+            <form action="process.php" method="post">
             <div class="form-group">
                 <label for="">ma tl</label>
-            <?php  echo  '<input type="text" name="" id="" value="'.$post[0].'" >' ?>
+            <?php  echo  '<input type="text" name="id" id="" value="'.$post['ma_tloai'].'" >' ?>
             </div>
             <div class="form-group">
                 <label for=""> tl</label>
-            <?php  echo  '<input type="text" name="" id="" value="' .$post[1].'">' ?>
+            <?php  echo  '<input type="text" name="name" id="" value="' .$post['ten_tloai'].'">' ?>
             </div>
             <div class="form-group">
-                <input type="submit" value="">
+                <input type="submit" value="Submit">
             </div>
+            </form>
+            
         </div>
     </div>
 </main>
